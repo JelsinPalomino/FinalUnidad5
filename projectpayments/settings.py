6ef1a8b0449f9e5e70144a9121ce1dac8df4e38d
JelsinPalomino <jstpalomino@hotmail.com>
2022-12-20 23:22:51 -0500

    settings

--- projectpayments/settings.py
+++ projectpayments/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t!jp881xrz#k_21i@5zdlog62)3^(0lt_m-%oouz-n5rk%&zyy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
