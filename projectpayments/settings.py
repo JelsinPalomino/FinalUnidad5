1a646853328f4d15ff0502abd0098467865dd10e
everyzzz <jquisper2@gmail.com>
2022-12-21 22:46:53 -0500

    modified static v1.0

--- projectpayments/settings.py
+++ projectpayments/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 import os
-STATIC_URL = '/static/'
+STATIC_URL = '../static'
 STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
